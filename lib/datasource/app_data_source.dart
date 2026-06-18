f8898a8409d0dd56bf497adf4e4fc575de2bf709
TaH00R <bgmiseason002@gmail.com>
2026-06-18 19:54:32 +0530

    Implemented the login method and fixed some errors

---
+++ lib/datasource/app_data_source.dart
@@ -0,0 +1,112 @@
+import 'package:bus_reservation_system/datasource/data_source.dart';
+import 'package:bus_reservation_system/models/app_user.dart';
+import 'package:bus_reservation_system/models/auth_response_model.dart';
+import 'package:bus_reservation_system/models/bus_model.dart';
+import 'package:bus_reservation_system/models/bus_reservation.dart';
+import 'package:bus_reservation_system/models/bus_route.dart';
+import 'package:bus_reservation_system/models/bus_schedule.dart';
+import 'package:bus_reservation_system/models/response_model.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class AppDataSource extends DataSource{
+  final String baseUrl = 'http://10.0.2.2:8080/api';
+
+  Map<String, String> get header => {
+    'Content-Type': 'application/json',
+  }
+
+  @override
+  Future<ResponseModel> addBus(Bus bus) {
+        throw UnimplementedError();
+  }
+
+  @override
+  Future<ResponseModel> addReservation(BusReservation reservation) {
+    // TODO: implement addReservation
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<ResponseModel> addRoute(BusRoute busRoute) {
+    // TODO: implement addRoute
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<ResponseModel> addSchedule(BusSchedule busSchedule) {
+    // TODO: implement addSchedule
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Bus>> getAllBus() {
+    // TODO: implement getAllBus
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<BusReservation>> getAllReservation() {
+    // TODO: implement getAllReservation
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<BusRoute>> getAllRoutes() {
+    // TODO: implement getAllRoutes
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<BusSchedule>> getAllSchedules() {
+    // TODO: implement getAllSchedules
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<BusReservation>> getReservationsByMobile(String mobile) {
+    // TODO: implement getReservationsByMobile
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<BusReservation>> getReservationsByScheduleAndDepartureDate(int scheduleId, String departureDate) {
+    // TODO: implement getReservationsByScheduleAndDepartureDate
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<BusRoute?> getRouteByCityFromAndCityTo(String cityFrom, String cityTo) {
+    // TODO: implement getRouteByCityFromAndCityTo
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<BusRoute?> getRouteByRouteName(String routeName) {
+    // TODO: implement getRouteByRouteName
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<BusSchedule>> getSchedulesByRouteName(String routeName) {
+    // TODO: implement getSchedulesByRouteName
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<AuthResponseModel?> login(AppUser user) async {
+        final url = '$baseUrl${'auth/login'}';
+        try{
+          final response = await http.post(
+          Uri.parse(url),
+          headers : header,
+          body: json.encode(user.toJson()),
+          );
+          final map = json.decode(response.body);
+        }catch(e){
+          print('Login error: $e');
+        }
+        throw UnimplementedError();
+  }
+
+}
--- lib/models/app_user.dart
+++ lib/models/app_user.dart
@@ -10,4 +10,22 @@ class AppUser {
     required this.password,
     this.role = 'Admin',
   });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'userName': userName,
+      'password': password,
+      'role': role,
+    };
+  }
+  
+  factory AppUser.fromJson(Map<String, dynamic> map) {
+    return AppUser(
+      id: map['id'],
+      userName: map['userName'],
+      password: map['password'],
+      role: map['role'],
+    );
+  }
 }
